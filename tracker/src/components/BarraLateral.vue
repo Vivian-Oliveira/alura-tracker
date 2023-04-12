7767e290f6148528fcaf459700c08d021c3703fb
Vivian de Oliveira Costa <Vivian-Oliveira>
2023-04-12 16:08:12 -0300

    feat: Add side menu component and global style.

---
+++ tracker/src/components/BarraLateral.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="logo" />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'BarraLateral'
+    })
+</script>
+
+<style scoped>
+    header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 30%;
+    height: 100vh;
+    }
+    @media only screen and (max-width: 768px) {
+        header {
+            height: auto;
+            width: 100%;
+        }
+}
+</style>
